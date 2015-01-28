4384d0fa5c4e9ba28c195183ab34cc6712be5a18
Claire Annan <claire.annan.sme@sonymusic.com>
2015-01-27 23:14:29 -0500

    Angular controller and module for contact form.

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-//Angular app model and controller 
+// define angular module/app
+var formApp = angular.module('formApp', []);
+
+// create angular controller and pass in $scope and $http
+function formController($scope, $http) {
+	$scope.formData = {};
+
+	$scope.processForm = function() {
+		$http({
+			method	: 'POST',
+			url			: 'process.php',
+			data 		: $.param($scope.formData),
+			headers : {'Content-Type': 'application/x-www-form-urlencoded'},
+		})
+		.success(function(data) {
+			// console.log(data);
+
+			if (!data.success) {
+				$scope.errorContactName = data.errors.contactName;
+				$scope.errorContactEmail = data.errors.contactEmail;
+				$scope.errorContactMessage = data.errors.contactMessage;
+			}
+			else {
+				$scope.message = data.message;
+			}
+		});	
+	};
+}
